6ed917a327ade126bbebe9bb697286b6d154ba56
Ajesh DS <ajeshd17@gmail.com>
2019-03-27 20:07:12 +0530

    Add CORS to API

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const Main = require("./routes");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("/attendance", Main)
