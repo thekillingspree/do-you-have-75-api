86ca8e97e98048c463074340badf884832d5dec6
Ajesh DS <ajeshd17@gmail.com>
2019-03-27 18:22:30 +0530

    Fix Typo Error

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("/attendance", Main)
 
-app.listen(process.env.PORT | 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('App Running');
 });
 
